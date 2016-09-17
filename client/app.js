94057287ca01fa99cf340108a5df9f1614df17d1
Michael Mead <michaelmead007@icloud.com>
2016-09-17 18:43:40 -0500

    set all musician properties on discover

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const app = angular.module('talent', ['ngRoute', 'ui.bootstrap'])
-  app.constant('apiUrl', "http://localhost:8000")
+app.constant('apiUrl', "http://localhost:8000")
 
-  app.config($httpProvider => {
+app.config($httpProvider => {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-  })
+})
 
 // angular.module('talent')
 app.factory('RootFactory', [
@@ -15,25 +15,25 @@ app.factory('RootFactory', [
     "apiUrl",
 
     ($http, $timeout, apiUrl) => {
-      let apiRoot = null;
-      let httpGet = $http.get(apiUrl);
-      let userCredentials = {};
+        let apiRoot = null;
+        let httpGet = $http.get(apiUrl);
+        let userCredentials = {};
 
-      return {
-        getApiRoot () {
-          return httpGet.then(res => res.data)
-        },
-        credentials (creds) {
-          if (creds) {
-            userCredentials = creds;
-          } else {
-            if (userCredentials.hasOwnProperty("password")) {
-              return window.btoa(`${userCredentials.username}:${userCredentials.password}`);
-            } else {
-              return false;
+        return {
+            getApiRoot() {
+                return httpGet.then(res => res.data)
+            },
+            credentials(creds) {
+                if (creds) {
+                    userCredentials = creds;
+                } else {
+                    if (userCredentials.hasOwnProperty("password")) {
+                        return window.btoa(`${userCredentials.username}:${userCredentials.password}`);
+                    } else {
+                        return false;
+                    }
+                }
             }
-          }
         }
-      }
     }
-  ])
+])
--- client/discover/discoverCtrl.js
+++ client/discover/discoverCtrl.js
@@ -2,8 +2,7 @@ angular.module('talent')
 .controller('DiscoverController', function($scope, $http) {
   // Get the events data.
   $http.get("http://localhost:8000/Musicians")
-       .then((res) => $scope.events = res.data)
-
+       .then((res) => $scope.artists = res.data)
 
 
 })
