f5ad1cc96d7e51820806c5fa89daf05801a48089
Michael Mead <michaelmead007@icloud.com>
2016-09-21 21:19:03 -0500

    changed discover

--- client/discover/discoverCtrl.js
+++ client/discover/discoverCtrl.js
@@ -1,9 +1,7 @@
 angular.module('talent')
-.controller('DiscoverController', function($scope, $http) {
+.controller('DiscoverController', function($scope, $http, $location) {
   // Get all the musicians data.
   $http.get("http://localhost:8000/Musicians")
        .then((res) => {$scope.artists = res.data})
-      //  console.log($scope.artists);
-
 
 })
